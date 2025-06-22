5614cdddc8319ffe329840b692a1d32a746fef3d
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-06-22 21:23:06 +0530

    10 - Event emitters with parameters

--- events/app.js
+++ events/app.js
@@ -2,8 +2,8 @@ const EventEmitter = require("events");
 
 const emitter = new EventEmitter();
 
-emitter.on("greet", (userName) => {
-  console.log(`Hello ${userName}`);
+emitter.on("greet", (userName, role) => {
+  console.log(`Hello ${userName}, You are ${role} of konoha`);
 });
 
-emitter.emit("greet", "Naruto");
+emitter.emit("greet", "Naruto", "Hokage");
